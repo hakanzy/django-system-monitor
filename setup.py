95331b1033fa1bcbdd4d1b50685e24f0afe7a981
Hakan OZAY <hakan.ozay@metglobal.com>
2013-05-28 16:18:55 +0300

    Updated path

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name='django-sysmon',
-    version='0.2',
+    version='0.1',
     author='Hakan OZAY',
     author_email='hakanzy@gmail.com',
     url='https://github.com/hakanzy/django-sysmon',
     description='View simple system statistics in django admin panel',
-    long_description=open('README.md').read(),
+    long_description=os.path.join(os.path.dirname(__file__), 'REAMDE.md'),
     packages=find_packages(exclude=[]),
     include_package_data=True,
     classifiers=[
